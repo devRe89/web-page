8a17bb6313569fcc74cc9301f56c076a15045a7e
devRe89 <develop.r89@gmail.com>
2021-10-27 10:34:02 -0300

    Up dev branch

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const FooterContainer = styled.footer`
+    width: 100%;
+    height: 45px;
+    overflow: hidden;
+    background: var(--grey);
+`;
 
 const Footer = () => {
     return ( 
-        <h2>Footer</h2>
+        <FooterContainer />
      );
 }
  
--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,10 +1,71 @@
 import React from 'react';
-
-const Layout = () => {
+import Header from './Header';
+import Footer from './Footer';
+import Head from 'next/head';
+import {Global, css} from '@emotion/react';
+const Layout = (props) => {
     return ( 
-        <h2>
-            Layout
-        </h2>
+       <>
+        <Global 
+            styles={css`
+                :root {
+                    --blue: #528ff5;
+                    --grey: ##f0f0f0
+                }
+                * {
+                    font-family: 'Roboto Slab', serif;
+                    font-weight: 500;
+                }
+
+                html {
+                    font-size: 62.5%;
+                    box-sizing: border-box;
+                }
+
+                *, *:before, *:after{
+                    box-sizing: inherit;
+                }
+
+                body {
+                    font-size: 1.6rem;
+                    line-height: 1.5;
+                    font-family: 'PT Sans' sans-serif;
+                }
+
+                a {
+                    text-decoration: none;
+                }
+
+                img {
+                    max-width: 100%;
+                    width: 100%;
+                }
+            `}
+        /> 
+
+        <Head>
+            <link 
+                href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap" 
+                rel="stylesheet" 
+            />
+            <link 
+                href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+                rel="stylesheet" 
+                integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
+                crossorigin="anonymous" 
+            />
+        </Head>
+
+        <Header />
+        
+        <main>
+            {props.children}
+        </main>
+
+        <Footer />
+
+       
+       </>
      );
 }
  
